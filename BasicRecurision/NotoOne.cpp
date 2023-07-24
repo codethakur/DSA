324117962287f8b70e2f86a396322b0feab4cbb3
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-07-24 14:11:10 +0530

    Update NotoOne.cpp

--- BasicRecurision/NotoOne.cpp
+++ BasicRecurision/NotoOne.cpp
@@ -1,1 +1,22 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+void NtoOne(int i, int n)
+{
+    if(i<1){ 
+        return;
+    }
+    cout<<i<<" ";
+    NtoOne(i-1,n);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    NtoOne(n,n);
+
+    return 0;
+}
