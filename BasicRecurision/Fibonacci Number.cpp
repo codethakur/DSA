d4f7fc86e860dcb8ba6b647fe2cdb28ac383b3c7
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-07-24 18:46:39 +0530

    Update Fibonacci Number.cpp
    509. Fibonacci Number
    LeetCode

--- BasicRecurision/Fibonacci Number.cpp
+++ BasicRecurision/Fibonacci Number.cpp
@@ -15,3 +15,16 @@ int fib(int n) {
 
         return 0;
     }
+/* Leetcode->509. Fibonacci Number
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)return 0;
+        if(n==1) return 1;  
+
+       int last = fib(n-1);
+       int slast = fib(n-2);  
+       return last+slast;
+    }
+};
+*/
