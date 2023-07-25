da6c253de6e6699e213a945bab4d1d506ca9b43d
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-07-26 00:05:08 +0530

    Add files via upload

---
+++ BasicRecurision/sortStack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+void insertTemp(std::stack<int>& st, int temp) {
+    if (st.empty() || st.top() < temp) {
+        st.push(temp);
+        return;
+    }
+
+    int val = st.top();
+    st.pop();
+    insertTemp(st, temp);
+    st.push(val);
+}
+
+void sortStack(std::stack<int>& stack) {
+    if (stack.size() == 0 || stack.size() == 1) return;
+
+    int temp = stack.top();
+    stack.pop();
+    sortStack(stack);
+    insertTemp(stack, temp);
+}
+
+int main() {
+    std::stack<int> myStack;
+
+    // Push elements into the stack (unsorted)
+    myStack.push(5);
+    myStack.push(2);
+    myStack.push(7);
+    myStack.push(1);
+    myStack.push(3);
+
+    // Sort the stack in descending order
+    sortStack(myStack);
+
+    // Print the sorted stack
+    while (!myStack.empty()) {
+        std::cout << myStack.top() << " ";
+        myStack.pop();
+    }
+
+    return 0;
+}
