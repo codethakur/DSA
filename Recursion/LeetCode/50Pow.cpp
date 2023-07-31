ee330628f236586ac0a0c41f72d199897f2ec3b8
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-07-31 13:26:43 +0530

    Update 50Pow.cpp

--- Recursion/LeetCode/50Pow.cpp
+++ Recursion/LeetCode/50Pow.cpp
@@ -20,5 +20,12 @@ public:
     }
     double myPow(double x, int n) {
         return binaryExp(x,(long)(long)n);
+        //if (n == 0) return 1;
+
+        // double temp = myPow(x, n / 2);
+        // if (n % 2 == 1 || n % 2 == -1)
+        //     return (n < 0) ? temp * temp * (1 / x) : temp * temp * x;
+        // else
+        //     return temp * temp;
     }
 };
