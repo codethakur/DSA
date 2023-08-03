45ab75e8c442e98a07e129fa63b8bdfe22e38e33
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-03 18:06:15 +0530

    Create subset.cpp

---
+++ Dynamic Programming/subset.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool knapsack(int arr[], int sum, int n)
+{
+    bool table[n + 1][sum + 1];
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < sum + 1; j++)
+        {
+            if (i == 0)
+                table[i][j] = false;
+            if (j == 0)
+                table[i][j] = true;
+            else if (arr[i - 1] <= j)
+                table[i][j] = (table[i - 1][j - arr[i - 1]] || table[i - 1][j]);
+            else
+                table[i][j] = table[i - 1][j];
+        }
+    }
+    return table[n][sum];
+}
+
+bool isSubsetSum(int arr[], int n, int sum)
+{
+    return knapsack(arr, sum, n);
+}
+
+int main()
+{
+    int arr[] = { 3, 34, 4, 12, 5, 2 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 9;
+
+    if (isSubsetSum(arr, n, sum))
+        cout << "Subset with the given sum exists." << endl;
+    else
+        cout << "No subset with the given sum exists." << endl;
+
+    return 0;
+}
