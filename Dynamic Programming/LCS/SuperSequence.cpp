7823afceb7c95ecb53a6ff84c828d42b239ef5a3
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-06 16:40:52 +0530

    Add files via upload

---
+++ Dynamic Programming/LCS/SuperSequence.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.length();
+    int n = text2.length();
+
+    int table[m + 1][n + 1];
+    for (int i = 0; i < m + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            if (i == 0 || j == 0)
+                table[i][j] = 0;
+        }
+    }
+    for (int i = 1; i < m + 1; i++) {
+        for (int j = 1; j < n + 1; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                table[i][j] = 1 + table[i - 1][j - 1];
+            } else
+                table[i][j] = max(table[i - 1][j], table[i][j - 1]);
+        }
+    }
+    return table[m][n];
+}
+
+int SuperSequence(string s1, string s2) {
+    int m = s1.length();
+    int n = s2.length();
+    
+    int lcs = longestCommonSubsequence(s1, s2);
+    
+    return (m + n) - lcs;
+}
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    cout << "Shortest Supersequence Length: " << SuperSequence(s1, s2) << endl;
+
+    return 0;
+}
