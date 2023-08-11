56f87d3ab2dbb158c124b7c8d1f38b936f6c4865
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-11 22:15:08 +0530

    Create 796. Rotate String.cpp

---
+++ String/leetcode/796. Rotate String.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+         if(s==goal)
+        {
+            return true;
+        }
+        queue<char>q1,q2;
+        for(int i=0; i<s.size(); i++)
+        {
+            q1.push(s[i]);
+        }
+        for(int j=0; j<goal.size(); j++)
+        {
+            q2.push(goal[j]);
+        }
+        int k = goal.size()-1;
+        while(k!=0)
+        {
+            char ch = q2.front();
+            q2.pop();
+            q2.push(ch);
+
+            if(q1==q2)
+            {
+                return true;
+            }
+            k--;
+        }
+        return false;
+    }
+};
+/*
+  class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+
+        if((s+s).find(goal)!=string::npos)
+        return true;
+        
+        return false;
+        
+    }
+};
+*/
