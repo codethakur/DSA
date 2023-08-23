798c43e82ae264e1fc2a687412ea23469949b585
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-23 23:42:14 +0530

    Update 115. Distinct Subsequences.cpp

--- String/leetcode/115. Distinct Subsequences.cpp
+++ String/leetcode/115. Distinct Subsequences.cpp
@@ -24,3 +24,29 @@ public:
         return (int)prev[m];
     }
 };
+/*
+    class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        vector<vector<unsigned int>> table(m + 1, vector<unsigned int>(n + 1, 0));
+
+        for (int i = 0; i <= m; ++i) {
+            table[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    table[i][j] = table[i - 1][j - 1] + table[i - 1][j];
+                } else {
+                    table[i][j] = table[i - 1][j];
+                }
+            }
+        }
+
+        return table[m][n];
+    }
+};
+*/
