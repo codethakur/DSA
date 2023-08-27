10d884de885e299262aeb39b342e305897ba7ae4
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-27 16:45:33 +0530

    Create Middle of Three.cpp

---
+++ Searching & Sorting/Middle of Three.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        if ((A > B && A < C) || (A < B && A > C))
+            return A;
+        else if ((B > A && B < C) || (B < A && B > C))
+            return B;
+        else
+            return C;
+    }
+};
