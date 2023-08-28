3d56bbf56b05fccf6668af5a411a33ebf4c4cf4a
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-28 18:10:30 +0530

    Update Find All Four Sum Numbers.cpp

--- Searching & Sorting/Find All Four Sum Numbers.cpp
+++ Searching & Sorting/Find All Four Sum Numbers.cpp
@@ -58,48 +58,70 @@ vector<vector<int>> fourSum(vector<int> &arr, int k) {
 // 1068 /1360
 // Time Limit Exceeded O(n^3)*log(m);
 //extra space
-vector<vector<int>> fourSum(vector<int> &arr, int k) {
-    int n = arr.size();
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
     vector<vector<int>> ans;
-    sort(arr.begin(), arr.end());
 
-    for (int i = 0; i < n - 3; i++) {
-        if (i > 0 && arr[i] == arr[i - 1])
-            continue;
+    // sort the given array:
+    sort(nums.begin(), nums.end());
 
-        for (int j = i + 1; j < n - 2; j++) {
-            if (j > i + 1 && arr[j] == arr[j - 1])
-                continue;
+    //calculating the quadruplets:
+    for (int i = 0; i < n; i++) {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-            int targetSum2 = k - (arr[i] + arr[j]);
-
-            int left = j + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int twoSum = arr[left] + arr[right];
-
-                if (twoSum == targetSum2) {
-                    vector<int> temp = {arr[i], arr[j], arr[left], arr[right]};
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
                     ans.push_back(temp);
+                    k++; l--;
 
-                    // Move the pointers and avoid duplicates
-                    left++;
-                    right--;
-                    while (left < right && arr[left] == arr[left - 1])
-                        left++;
-                    while (left < right && arr[right] == arr[right + 1])
-                        right--;
-                } else if (twoSum < targetSum2) {
-                    left++;
-                } else {
-                    right--;
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
                 }
+                else if (sum < target) k++;
+                else l--;
             }
         }
     }
 
     return ans;
+}
+
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
+    for (auto it : ans) {
+        cout << "[";
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+
 // }
 // O(n^2 * n) ~ n^3
 
