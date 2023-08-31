e6fc9ea6f4df9273d11c18434c8b7d02d4e9f0ef
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-08-31 12:38:02 +0530

    Update MoveZeroes.cpp
    Optimal solution

--- Array/MoveZeroes.cpp
+++ Array/MoveZeroes.cpp
@@ -20,6 +20,26 @@ vector<int>MoveZeroes(int n, vector<int>&nums)
     }
     return nums;
 }
+/*
+    int j=-1;
+for(int i=0; i<nums.size(); i++)
+{
+    if(nums[i]==0){
+        j=i;        //Time O(x);
+        break;
+    }
+}
+for(int i=j+1; i<n; i++)
+{
+    if(nums[i]!=0)
+    {
+        swap(nums[i],nums[j]); //Time O(n-x);
+        j++;
+    }
+}
+//Time = O(x) + O(n-x) x+(-x)=> O(N) TC with no extra space;
+return nums;
+*/
 
 int main()
 {
