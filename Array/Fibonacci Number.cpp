2b5ef600a7804399a864f482baa9dfe7207c090f
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-09-01 18:17:46 +0530

    Update Fibonacci Number.cpp
    time complexity to O(n),

--- Array/Fibonacci Number.cpp
+++ Array/Fibonacci Number.cpp
@@ -9,3 +9,24 @@ public:
        return last+slast;
     }
 };
+//```````````````````````````````````````
+class Solution {
+public:
+    int fib(int n) {
+        // Create a memoization table to store already computed values
+        vector<int> memo(n + 1, -1);
+        return fibMemo(n, memo);
+    }
+
+private:
+    int fibMemo(int n, vector<int>& memo) {
+        if (n <= 1)
+            return n;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+        return memo[n];///time complexity to O(n)
+    }
+};
