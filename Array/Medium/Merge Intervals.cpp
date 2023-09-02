18c0a46a4c988e46926f10db5af8824ed9e03fd1
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-09-02 16:48:52 +0530

    Create Merge Intervals.cpp

---
+++ Array/Medium/Merge Intervals.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+         int n = arr.size(); // size of the array
+
+    //sort the given intervals:
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) { // select an interval:
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        //Skip all the merged intervals:
+        if (!ans.empty() && end <= ans.back()[1]) {
+            continue;
+        }
+
+        //check the rest of the intervals:
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j][0] <= end) {
+                end = max(end, arr[j][1]);
+            }
+            else {
+                break;
+            }
+        }
+        ans.push_back({start, end});
+    }
+    return ans;
+    }
+};
+//```````````````````````````````````
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n= intervals.size();
+        vector<vector<int>>ans;
+        sort(intervals.begin(), intervals.end());
+        for(int i=0; i<n; i++){
+            if(ans.empty() ||intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
+//O(N*logN) + O(N), where N = the size of the given array.
