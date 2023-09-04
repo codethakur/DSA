30804abde1c5898d1dfba8fa10fcbfc1e5102fed
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-09-04 12:10:21 +0530

    Update Merge Intervals.cpp

--- Array/Medium/Merge Intervals.cpp
+++ Array/Medium/Merge Intervals.cpp
@@ -50,3 +50,59 @@ public:
     }
 };
 //O(N*logN) + O(N), where N = the size of the given array.
+
+//`````````````````````````````````````````
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+        // Sort intervals based on the start of each interval
+        std::sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
+            return a[0] < b[0];
+        });
+
+        std::vector<std::vector<int>> merged;
+
+        for (const std::vector<int>& interval : intervals) {
+            if (merged.empty() || interval[0] > merged.back()[1]) {
+                // If the current interval doesn't overlap with the previous merged interval, add it as is.
+                merged.push_back(interval);
+            } else {
+                // If there is an overlap, merge the current interval with the last merged interval.
+                merged.back()[1] = std::max(merged.back()[1], interval[1]);
+            }
+        }
+
+        return merged; //  O(n log n), 
+    }
+};
+//Time Complexity:  O(n log n),
+// Space Complexity: O(n) because the merged vector can potentially contain all n intervals. 
+//``````````````````````````````````
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
+        int idx = 0;
+
+       for(int i=1; i<n; i++)
+       {
+           if(intervals[i][0]<=intervals[idx][1])
+           {
+               intervals[idx][1] = std::max(intervals[idx][1], intervals[i][1]);
+           }
+           else{
+               idx++;
+               intervals[idx] = intervals[i];
+           }
+       } 
+       intervals.resize(idx+1);
+       return intervals;
+    }
+};
+//Time Complexity:  O(n log n),
+// Space Complexity: O(1)
