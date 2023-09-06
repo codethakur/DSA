a95cb696dd16fc032c6f7f0e3a481a83ebfbc019
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-09-06 23:14:01 +0530

    Create 3Sum Closest.cpp

---
+++ Array/Medium/3Sum Closest.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+class Solution {
+public:
+    int threeSumClosest(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n < 3) {
+            return 0;
+        }
+
+        std::sort(nums.begin(), nums.end());
+        int closestSum = INT_MAX;
+        int minDiff = INT_MAX;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    int diff = std::abs(sum - target);
+
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        closestSum = sum;
+                    }
+                }
+            }        //O(n^3);
+        }
+
+        return closestSum;
+    }
+};
+//~~~~~~~~~~~~~`````````````````````~~~~~~~~~~~~~~~~~~~```````````````````~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
