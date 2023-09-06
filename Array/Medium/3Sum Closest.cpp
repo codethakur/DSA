64b9cc1d21ae69ead0b9499e9a827d60b0b2382f
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-09-06 23:28:09 +0530

    Update 3Sum Closest.cpp

--- Array/Medium/3Sum Closest.cpp
+++ Array/Medium/3Sum Closest.cpp
@@ -32,4 +32,45 @@ public:
     }
 };
 //~~~~~~~~~~~~~`````````````````````~~~~~~~~~~~~~~~~~~~```````````````````~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+class Solution {
+public:
+    int threeSumClosest(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n < 3) {
+            return 0;
+        }
+
+        std::sort(nums.begin(), nums.end());
+        int closestSum = INT_MAX;
+        int minDiff = INT_MAX;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = std::abs(sum - target);
+
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }            // time complexity of O(n^2)
+
+        return closestSum;
+    }
+};
 
