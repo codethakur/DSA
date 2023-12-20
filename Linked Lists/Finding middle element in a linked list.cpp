3d0667e99707a8973b9ea0441c84e9d45bdc94fa
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-12-20 17:25:54 +0530

    Update Finding middle element in a linked list.cpp
    using 2 pointer

--- Linked Lists/Finding middle element in a linked list.cpp
+++ Linked Lists/Finding middle element in a linked list.cpp
@@ -37,3 +37,23 @@ class Solution{
         return temp->data;  // Return the data of the middle node, not the pointer
     }
 };
+
+/*``````````````````Approch 2``````````````````````````````*/
+int getMiddle(Node *head)
+    {
+        // Your code here
+        if (head == NULL)
+        {
+            return -1; // Return -1 for an empty list
+        }
+    
+        Node *slow = head;
+        Node *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+    
+        return slow->data;
+    }
