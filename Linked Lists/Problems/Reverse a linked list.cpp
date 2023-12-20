a23c3b4f4f68ec89b85aed4ddb84fcc53143c382
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-12-20 15:41:28 +0530

    Create Reverse a linked list.cpp
    GFG

---
+++ Linked Lists/Problems/Reverse a linked list.cpp
@@ -0,0 +1,36 @@
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+/*`````````````````````Approch 1St```````````````````````````*/
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        Node* prevNode = NULL;
+        Node* currNode = head;
+        Node* NextNode=NULL;
+        while(currNode!=NULL)
+        {
+            NextNode = currNode->next;
+            currNode->next=prevNode;
+            prevNode= currNode;
+            currNode=NextNode;
+            
+        }
+        return prevNode;
+    }
+    
+};
