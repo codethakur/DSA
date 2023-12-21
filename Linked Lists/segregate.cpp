27c9f77cda43d17e5fd666907fb205a315c6e81c
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-12-21 22:29:23 +0530

    Given a linked list of 0s, 1s and 2s, sort it

---
+++ Linked Lists/segregate.cpp
@@ -0,0 +1,74 @@
+ struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    void insertAtTail(Node* &tail, Node* curr ) {
+        tail -> next = curr;
+        tail = curr;
+    }
+
+    Node* segregate(Node *head) {
+        
+        // Add code here
+        Node* zeroHead = new Node(-1);
+        Node* zeroTail = zeroHead;
+        Node* oneHead = new Node(-1);
+        Node* oneTail = oneHead;
+        Node* twoHead = new Node(-1);
+        Node* twoTail = twoHead;
+        
+        Node* curr = head;
+        
+        // create separate list 0s, 1s and 2s
+        while(curr != NULL) {
+            
+            int value = curr -> data;
+            
+            if(value == 0) {
+                insertAtTail(zeroTail, curr);
+            }
+            else if(value == 1) {
+                insertAtTail(oneTail, curr);
+            }
+            else if(value == 2) {
+                insertAtTail(twoTail, curr);
+            }       
+            curr = curr -> next;
+        }
+        
+        //merge 3 sublist
+        
+        // 1s list not empty
+        if(oneHead -> next != NULL) {
+            zeroTail -> next = oneHead -> next;
+        }
+        else {
+            //1s list -> empty
+            zeroTail -> next = twoHead -> next;
+        }
+        
+        oneTail -> next = twoHead -> next;
+        twoTail -> next = NULL;
+        
+    	//setup head 
+        head = zeroHead -> next;
+        
+        //delete dummy nodes
+        delete zeroHead;
+        delete oneHead;
+        delete twoHead;
+        
+        return head;
+        
+    }
+};
