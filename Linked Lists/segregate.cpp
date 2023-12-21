46f1a4d449d374f7e3e13bbd27576706ebaaa857
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-12-21 22:31:28 +0530

    Update segregate.cpp

--- Linked Lists/segregate.cpp
+++ Linked Lists/segregate.cpp
@@ -1,4 +1,4 @@
- struct Node {
+/* struct Node {
     int data;
     struct Node *next;
     Node(int x) {
@@ -69,6 +69,68 @@ class Solution
         delete twoHead;
         
         return head;
+        
+    }
+};
+///////////// Another Way ///////////////////////
+
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        
+        // Add code here
+     int zeroCount = 0;
+     int oneCount = 0;
+     int twoCount = 0;
+ 
+     Node* temp = head;
+ 
+     // Count the occurrences of 0s, 1s, and 2s
+     while (temp != NULL) {
+         if (temp->data == 0)
+             zeroCount++;
+         else if (temp->data == 1)
+             oneCount++;
+         else if (temp->data == 2)
+             twoCount++;
+ 
+         temp = temp->next;
+     }
+ 
+     temp = head;
+ 
+     // Update the linked list based on the counts
+     while (temp != NULL) {
+         if (zeroCount != 0) {
+             temp->data = 0;
+             zeroCount--;
+         } else if (oneCount != 0) {
+             temp->data = 1;
+             oneCount--;
+         } else if (twoCount != 0) {
+             temp->data = 2;
+             twoCount--;
+         }
+ 
+         temp = temp->next;
+     }
+ 
+     return head;
         
     }
 };
