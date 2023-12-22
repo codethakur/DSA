a6d1a4944b8545571ca87515d8765b0c2844d675
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2023-12-23 00:20:51 +0530

    CloneLL.cpp

--- Linked Lists/Clone.cpp
+++ Linked Lists/Clone.cpp
@@ -1,3 +1,59 @@
+class Solution
+{
+    private:
+    void insertAtTailNode(Node* &head, Node* &tail, int data) {
+        Node* newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    public:
+    Node *copyList(Node *head)
+    {
+        //Write your code here
+        if (head == NULL) {
+            return NULL;
+        }
+
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+
+        Node* temp = head;
+        while (temp != NULL) {
+            insertAtTailNode(cloneHead, cloneTail, temp->data);
+            temp = temp->next;
+        }
+
+        unordered_map<Node*, Node*> OriginalNodeToclone;
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+
+        while (originalNode != NULL) {
+            OriginalNodeToclone[originalNode] = cloneNode;
+            originalNode = originalNode->next;
+            if (originalNode != NULL) {
+                cloneNode = cloneNode->next;
+            }
+        }
+
+        originalNode = head;
+        cloneNode = cloneHead;
+
+        while (originalNode != NULL) {
+            cloneNode->arb = OriginalNodeToclone[originalNode->arb];
+            originalNode = originalNode->next;
+            cloneNode = cloneNode->next;
+        }
+
+        return cloneHead;        
+    }
+
+};
+/////////////////////////////////////////
 class Solution {
     void insertAtTail(Node* &head, Node* &tail, int d) {
         Node* newNode = new Node(d);
