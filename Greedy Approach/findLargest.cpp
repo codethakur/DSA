8fd524a259e875afe8e14dbfe210a4642f167b22
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-01-10 18:16:47 +0530

    Largest number possible

---
+++ Greedy Approach/findLargest.cpp
@@ -0,0 +1,25 @@
+class Solution{
+public:
+    string findLargest(int N, int S){
+        // code here
+        string ans = "";
+        
+        if(S==0 && N>1){
+            return "-1";
+        }
+        for(int i=0; i<N; i++)
+        {
+            if(S>9){
+                ans+='9';
+                S -= 9;
+            }else{
+                ans += to_string(S);
+                S-=S;
+            }
+        }
+        if(S!=0){
+            return "-1";
+        }
+        return ans;
+    }
+};
