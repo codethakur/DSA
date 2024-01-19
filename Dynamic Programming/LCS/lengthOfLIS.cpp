44b4d26dd76f4471fe51c265dd420aa763f44172
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-01-19 14:58:28 +0530

    300. Longest Increasing Subsequence

--- Dynamic Programming/LCS/lengthOfLIS.cpp
+++ Dynamic Programming/LCS/lengthOfLIS.cpp
@@ -5,7 +5,7 @@ public:
 
         for (int i = 0; i < n + 1; i++) {
             for (int j = 0; j < m + 1; j++) {
-                if (i == 0 || j == 0)
+                if (i == 0 || j == 0) 
                     t[i][j] = 0;
             }
         }
