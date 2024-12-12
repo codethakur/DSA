88cbd9ce7003eedc24cb3198fdf6fb6dcdb7944f
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2024-12-12 20:37:31 +0530

    Create stack.cpp

---
+++ Stack 2.O/Source Files/stack.cpp
@@ -0,0 +1,6 @@
+#include "stack.h"
+
+template Stack<char>* createStack<char>(int);
+template void push<char>(Stack<char>*, char);
+template bool isEmpty<char>(Stack<char>*);
+template char pop<char>(Stack<char>*);
